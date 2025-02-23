eed6fd0c431a3411b23894fd4add311fa3f75bf4
timosha <tinaatimkovaa@gmail.com>
2025-02-22 22:44:14 -0500

    added interactivity to tab menu

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -1,13 +1,17 @@
-const menu = document.querySelector('.tabs');
-const tabs = document.querySelectorAll('.tabs a');
-
-menu.addEventListener('click', function(e) {
-    const selected_tab = e.target;
+document.querySelector('.tabs').addEventListener('click', function(e) {
+    const tabs = document.querySelectorAll('.tabs a');
+    const active_tab = e.target;
+    const cards = document.querySelectorAll('.card');
 
     tabs.forEach(tab => { 
-        if (tab != selected_tab && tab.classList.contains('tab_active')) 
+        if (tab != active_tab && tab.classList.contains('tab_active')) 
             tab.classList.remove('tab_active');
 
-        if (tab == selected_tab) tab.classList.add('tab_active');
+        if (tab == active_tab) tab.classList.add('tab_active');
     });
+
+    cards.forEach(card => {
+        if (card.classList.contains(e.target.id) || (e.target.id == 'all-components')) card.style.display = 'flex';
+        else card.style.display = 'none';
+    }); 
 })
