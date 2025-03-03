9c621c7f9062b20c850733e98f765dbb8c5e2f34
timosha <tinaatimkovaa@gmail.com>
2025-03-03 01:31:57 -0500

    working on the tab menu for component page

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -1,7 +1,15 @@
-document.querySelector('.tabs').addEventListener('click', function(e) {
-    const tabs = document.querySelectorAll('.tabs a');
-    const active_tab = e.target;
+document.querySelectorAll('.tabs__menu').forEach(menu => menu.addEventListener('click', function(e) {
     const cards = document.querySelectorAll('.card');
+    switchTab(e);
+    cards.forEach(card => {
+        if (card.classList.contains(e.target.id) || (e.target.id == 'all-components')) card.style.display = 'flex';
+        else card.style.display = 'none';
+    }); 
+}));
+
+const switchTab = function(e) {
+    const tabs = document.querySelectorAll('.tab');
+    const active_tab = e.target;
 
     tabs.forEach(tab => { 
         if (tab != active_tab && tab.classList.contains('tab_active')) 
@@ -9,9 +17,4 @@ document.querySelector('.tabs').addEventListener('click', function(e) {
 
         if (tab == active_tab) tab.classList.add('tab_active');
     });
-
-    cards.forEach(card => {
-        if (card.classList.contains(e.target.id) || (e.target.id == 'all-components')) card.style.display = 'flex';
-        else card.style.display = 'none';
-    }); 
-})
+}
