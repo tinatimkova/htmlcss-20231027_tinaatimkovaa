a2a2e47ef936aa3221389543dba0a8bd83a47065
timosha <tinaatimkovaa@gmail.com>
2025-03-04 00:20:31 -0500

    added custom form elements to template.html

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -1,20 +1,32 @@
-document.querySelectorAll('.tabs__menu').forEach(menu => menu.addEventListener('click', function(e) {
+const switchTab = function(e) {
+    const tabs = document.querySelectorAll('.tab');
+    const activeTab = e.target.closest('.tab');
+
+    tabs.forEach(tab => { 
+        if (tab != activeTab && tab.classList.contains('tab_active')) 
+            tab.classList.remove('tab_active');
+
+        if (tab == activeTab) tab.classList.add('tab_active');
+    });
+}
+
+const showCards = function(e) {
     const cards = document.querySelectorAll('.card');
-    switchTab(e);
+
     cards.forEach(card => {
         if (card.classList.contains(e.target.id) || (e.target.id == 'all-components')) card.style.display = 'flex';
         else card.style.display = 'none';
     }); 
-}));
-
-const switchTab = function(e) {
-    const tabs = document.querySelectorAll('.tab');
-    const active_tab = e.target;
-
-    tabs.forEach(tab => { 
-        if (tab != active_tab && tab.classList.contains('tab_active')) 
-            tab.classList.remove('tab_active');
-
-        if (tab == active_tab) tab.classList.add('tab_active');
-    });
 }
+
+const navTabs =  document.querySelector('.tabs .tabs__menu');
+const componentTabs = document.querySelector('.component .tabs__menu');
+
+if (navTabs) navTabs.addEventListener('click', function(e) {
+    switchTab(e);
+    showCards(e);
+});
+
+if (componentTabs) componentTabs.addEventListener('click', function(e) {
+    switchTab(e);
+});
